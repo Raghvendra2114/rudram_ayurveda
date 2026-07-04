a670470ed459585958bd2ff86444a4df3047c827
Raghvendra Jaiswal <raghvendrajaiswal2114@gmail.com>
2026-07-05 03:08:46 +0530

    Fixed SMTP timeout

--- server/config/mailer.js
+++ server/config/mailer.js
@@ -1,18 +1,17 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
   host: "smtp.gmail.com",
-  port: 587,
-  secure: false,
-  family: 4, // FORCE IPv4 FIX
+  port: 465,
+  secure: true,
+  family: 4,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
   },
-  tls: {
-    rejectUnauthorized: false
-  }
+  connectionTimeout: 30000,
+  greetingTimeout: 30000,
+  socketTimeout: 30000
 });
 
 async function sendAdminEmail(mailOptions) {
